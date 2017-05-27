644f3d3e7f5b828ff0c1c371849952ca5606fa01
STEPER <step@step>
2017-05-27 18:53:31 +0900

    homework 1

--- STEP/step1/step1_1.py
+++ STEP/step1/step1_1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # coding: UTF-8
-
 import itertools
 
 # make a dictionary
@@ -10,7 +9,6 @@ f.close()
 words = file.split('\n')
 
 dict = {}
-
 for word in words:
     if len(word) >= 3 and len(word) <= 16:
         sorted_word = list(word)
@@ -20,13 +18,10 @@ for word in words:
             dict[sorted_word] = [word]
         else:
             dict[sorted_word].append(word)
-point = {'a':1, 'b':1, 'd':1, 'e':1, 'g':1, 'i':1,
-         'n':1, 'o':1, 'r':1, 's':1, 't':1, 'u':1,
-         'c':2, 'f':2, 'h':2, 'l':2, 'm':2, 'p':2,
-         'v':2, 'w':2, 'y':2, 'j':3, 'k':3, 'q':3,
-         'x':3, 'z':3}
+point = {'a':1, 'b':1, 'd':1, 'e':1, 'g':1, 'i':1, 'n':1, 'o':1, 'r':1, 's':1, 't':1, 'u':1,
+         'c':2, 'f':2, 'h':2, 'l':2, 'm':2, 'p':2, 'v':2, 'w':2, 'y':2,
+         'j':3, 'k':3, 'q':3, 'x':3, 'z':3}
 print('dict prepared')
-
 
 def find(l, n):
     candidate = list(itertools.combinations(l, n))
@@ -38,19 +33,15 @@ def find(l, n):
             return True
     return False
 
-
-
 # in dictionary or not
 while True:
     input = list(raw_input('alphabets:'))
-
+    
     score = []
     for i in range(len(input)):
         score.append((input[i], point[input[i]]))
     score = sorted(score, key=lambda x: x[1], reverse=True)
-    #print(score)
     alphabets = [x[0] for x in score]
-    #print(alphabets)
 
     for i in range(len(alphabets)):
         f = find(alphabets, len(alphabets)-i)
